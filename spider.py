97d037ce27243e842c3b92c64d218a5bdaf4e26b
cwx2810 <cwx2810@163.com>
2017-09-12 10:40:02 -0700

    获取单页

---
+++ spider.py
@@ -0,0 +1,20 @@
+import requests
+from requests.exceptions import RequestException
+
+#获取单页
+def get_one_page(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except RequestException:
+        return None
+
+def main():
+    url = 'http://maoyan.com/board/4?'
+    html = get_one_page(url)
+    print(html)
+
+if __name__ == '__main__':
+    main()
