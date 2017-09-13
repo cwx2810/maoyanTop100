c563d7b1be6578c8658d3d41fb12bd63e38f5d37
cwx2810 <cwx2810@163.com>
2017-09-13 13:46:25 -0700

    猫眼被玩坏了，有了防御，改爬豆瓣，完成解析网页，格式化输出

--- spider.py
+++ spider.py
@@ -1,5 +1,6 @@
 import requests
 from requests.exceptions import RequestException
+import re
 
 #获取单页
 def get_one_page(url):
@@ -11,10 +12,23 @@ def get_one_page(url):
     except RequestException:
         return None
 
+#解析网页
+def parse_one_page(html):
+    #通过re将正则表达式字符串转换成正则表达式对象，做到这里发现猫眼封了爬虫，改爬豆瓣
+    pattern = re.compile('<li>.*?item.*?title">(.*?)</span>.*?inq">(.*?)</span>.*?</li>', re.S)
+    items = re.findall(pattern, html)
+    #格式化
+    for item in items:
+        yield{
+            'name': item[0],
+            'comments': item[1]
+        }
+
 def main():
-    url = 'http://maoyan.com/board/4?'
+    url = 'https://movie.douban.com/top250'
     html = get_one_page(url)
-    print(html)
+    for item in parse_one_page(html):
+        print(item)
 
 if __name__ == '__main__':
     main()
